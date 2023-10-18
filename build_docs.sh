11b3bf532188e1e38f88f17987d2f3225231b594
billy <tyrrell.simon@gmail.com>
2023-10-18 17:55:30 +0100

    wip on automating doc building

--- build_docs.sh
+++ build_docs.sh
@@ -1,30 +1,64 @@
 #!/bin/bash
 
+ROOT=..
+OUTPUT_DIR=docs
+OUTPUT_FILE=${OUTPUT_DIR}/index.md
+
+
+BuildDocForContent () 
+{
+	i=${ROOT}/$1/readme.md; 
+	n=$( basename $( dirname "$i" ) ); 
+	n=${n/-/ }; 
+	to="$1/$( basename $( dirname "$i")).md"; 
+	echo "> $n"; 
+	cp "$i" "${OUTPUT_DIR}/${to}"; 
+	echo " * [$n](${to})" >> ${OUTPUT_FILE}; 
+}
+
+
+
 #
-# $1 is the directory name that we're copying and building teh docs for
+# $1 is the directory name that we're copying and building the docs for
 # $2 is the heading used for the list of entries
 BuildDocsForContent () 
 {
-	mkdir -p $1
+	mkdir -p ${OUTPUT_DIR}/$1
 	echo "" >> ${OUTPUT_FILE}
 	echo "## $2" >> ${OUTPUT_FILE}
+
+	if [[ $3 = "true" ]]
+	then
 	for i in ${ROOT}/$1/*/readme.md; 
-		do n=$( basename $( dirname "$i" ) ); n=${n/-/ }; to="$1/$( basename $( dirname "$i")).md"; echo "> $n"; cp "$i" "${to}"; echo " * [$n](${to})" >> ${OUTPUT_FILE}; 
+		do n=$( basename $( dirname "$i" ) ); n=${n/-/ }; to="$1/$( basename $( dirname "$i")).md"; echo "> $n"; cp "$i" "${OUTPUT_DIR}/${to}"; echo " * [$n](${to})" >> ${OUTPUT_FILE}; 
 	done
+	else
+		i=${ROOT}/$1/readme.md; n=$( basename $( dirname "$i" ) ); n=${n/-/ }; to="$1/$( basename $( dirname "$i")).md"; echo "> $n"; cp "$i" "${OUTPUT_DIR}/${to}"; echo " * [$n](${to})" >> ${OUTPUT_FILE}; 
+	fi
+
 }
 
-OUTPUT_FILE=contents2.md
-ROOT=..
 
 
 # Clear the output file
 truncate -s 0 ${OUTPUT_FILE}
 
 
-BuildDocsForContent services Services
-BuildDocsForContent libs Libraries
-BuildDocsForContent servers Servers
-BuildDocsForContent clients Clients
+BuildDocsForContent services Services true
+BuildDocsForContent libs Libraries true
+
+# Lucene
+#mkdir -p ${OUTPUT_DIR}/lucene
+#cp ${ROOT}/lucene/readme.md ${OUTPUT_DIR}/lucene/lucene.md
+#echo "> lucene"
+#echo "" >> ${OUTPUT_FILE}
+#echo "## lucene" >> ${OUTPUT_FILE}
+#echo " * [lucene](lucene/lucene.md)" >> ${OUTPUT_FILE}
+
+BuildDocsForContent lucene Lucene false
+
+BuildDocsForContent servers Servers true
+BuildDocsForContent clients Clients true
 
 
 
